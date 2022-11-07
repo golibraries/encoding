5c2fbb605ada50745d228025bf889c65eaf058c1
lizongti <lzongti@gmail.com>
2022-11-07 18:46:04 +0800

    add proto

--- json.go
+++ json.go
@@ -1,15 +1,15 @@
 package encoding
 
 import (
-	originJSON "encoding/json"
+	j "encoding/json"
 )
 
 type json struct{}
 
 func (json) Marshal(v interface{}) ([]byte, error) {
-	return originJSON.Marshal(v)
+	return j.Marshal(v)
 }
 
 func (json) Unmarshal(data []byte, v interface{}) error {
-	return originJSON.Unmarshal(data, v)
+	return j.Unmarshal(data, v)
 }
